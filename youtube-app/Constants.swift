b90e51629a2467d4fd221a0c7aeb15dce8ab0555
Ismatulla Mansurov <47342870+sapoepsilon@users.noreply.github.com>
2022-01-07 16:47:38 -0700

    Update Constants.swift

--- youtube-app/Constants.swift
+++ youtube-app/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Constants {
     
-    static var API_KEY = "AIzaSyDhCZQyyGwoKkNsFbQ0Dbtb9Ot7YvPzIUA"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "PLEghovcPVHbuUrBikUJEICGStJHtU6eHn"
 static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
